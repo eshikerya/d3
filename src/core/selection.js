cc82345f6cc66f19738b92e513a7ed8d4276d21c
Jacky Shikerya <shikerya@gmail.com>
2012-10-10 14:08:47 +0300

    selectAll(foreignObject) webkit issue fix

--- src/core/selection.js
+++ src/core/selection.js
@@ -4,7 +4,11 @@ function d3_selection(groups) {
 }
 
 var d3_select = function(s, n) { return n.querySelector(s); },
-    d3_selectAll = function(s, n) { return n.querySelectorAll(s); },
+    d3_selectAll = function(s, n) {
+		return s == 'foreignObject' && /webkit/i.test(window.navigator.userAgent)
+			? n.getElementsByTagName(s)
+			: n.querySelectorAll(s);
+	},
     d3_selectRoot = document.documentElement,
     d3_selectMatcher = d3_selectRoot.matchesSelector || d3_selectRoot.webkitMatchesSelector || d3_selectRoot.mozMatchesSelector || d3_selectRoot.msMatchesSelector || d3_selectRoot.oMatchesSelector,
     d3_selectMatches = function(n, s) { return d3_selectMatcher.call(n, s); };
